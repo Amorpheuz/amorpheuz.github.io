13d007d3c8110ad5e0e0c6c8907b740a08ff2064
Amorpheuz <mail2ypd@gmail.com>
2020-03-03 22:48:01 +0530

    init terminal

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# amorpheuz.github.io
+# gatsby-starter-terminal
+
+This is a starter repo for [gatsby-theme-terminal](https://github.com/PaulieScanlon/gatsby-theme-terminal)
+
+## 👁️ Preview
+
+- [Live Demo](https://gatsby-theme-terminal.netlify.com/)
---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    name: "gatsby-starter-terminal",
+    description: "A Gatsby starter for @pauliescanlon/gatsby-theme-terminal",
+    keywords: ["tech", "blog", "boop"],
+    siteUrl: "https://gatsby-starter-terminal.netlify.com",
+    siteImage: "terminal-open-graph-image.jpg",
+    profileImage: ``,
+    lang: `en`,
+    config: {
+      sidebarWidth: 280
+    }
+  },
+  plugins: ["@pauliescanlon/gatsby-theme-terminal"]
+};
