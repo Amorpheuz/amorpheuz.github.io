82e12c9fbb0c40f05cb26f17ce1b1c2b61a04a2a
Amorpheuz <mail2ypd@gmail.com>
2020-06-06 21:23:02 +0530

    setup light theme - fix list>code element color issues - move social icons to component - shadow Nav component - create ThemeSwitch component

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -11,11 +11,28 @@ export default {
     secondary: "#08d9d6",
     success: "#27FB6B",
     background: "#252a34",
-    surface: "#2E303E"
+    surface: "#2E303E",
+    modes: {
+      light: {
+        text: "#1b262c",
+        muted: "#311d3f",
+        primary: "#384259",
+        secondary: "#f73859",
+        success: "#48ADA9",
+        background: "#f9f7f7",
+        surface: "#EDE8E8"
+      }
+    }
   },
   styles: {
     ...baseTheme.styles,
     p: {
+      code: {
+        ...baseTheme.styles.p.code,
+        color: "inherit"
+      }
+    },
+    li: {
       code: {
         ...baseTheme.styles.p.code,
         color: "inherit"
